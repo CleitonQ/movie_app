a558197ad1238a2252ad5462af258bdf4662eac3
CleitonQ <cleitonqueirozc@gmail.com>
2025-09-25 23:54:29 -0300

    Resolvendo conexão com a API

--- lib/data/movie_api.dart
+++ lib/data/movie_api.dart
@@ -3,7 +3,7 @@ import 'package:movie_app/data/models/movie.dart';
 
 class MovieApi {
   final Dio _dio = Dio(
-      BaseOptions(baseUrl: 'https://apifilmes.webevolui.com')
+      BaseOptions(baseUrl: 'https://apifilmes.webevolui.com/swagger/index.html')
   ); // Dio
 
   Future<List<Movie>> getMovies({int skip = 0, int take = 20}) async {
